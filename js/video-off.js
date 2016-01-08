fb3c4cc6dc591f0f6268f4a0469f9f0ee88de9c0
Delinda <delindafrazier@gmail.com>
2016-01-07 22:52:06 -0500

    Found Solution to turn off video
    Script source:
    http://stackoverflow.com/questions/26497707/vimeo-sound-keeps-playing-in-bootstrap-modal-after-closing-the-modal

--- js/video-off.js
+++ js/video-off.js
@@ -1,7 +1,29 @@
 //Javascript
+//Script source: http://stackoverflow.com/questions/26497707/vimeo-sound-keeps-playing-in-bootstrap-modal-after-closing-the-modal
 
-$(window).load(function(){
-jQuery('#cnm-modal').on('hidden.bs.modal', function (e) {
-jQuery('#cnm-modal video').attr("src", jQuery("#cnm-modal video").removeAttr("src"));
-});
-});
+
+   $('#michelle-modal').on('hide.bs.modal', function () {
+
+   // get the source of the iframe and save it
+     var src = $(this).find('iframe').attr('src');
+
+   // remove the src from the iframe
+     $("iframe#michelle-iframe").attr('src','');
+
+   // re-add the 
+     $("iframe#michelle-iframe").attr('src', src);
+
+   });
+
+   $('#cnm-modal').on('hide.bs.modal', function () {
+
+   // get the source of the iframe and save it
+     var src = $(this).find('iframe').attr('src');
+
+   // remove the src from the iframe
+     $("iframe#cnm-iframe").attr('src','');
+
+   // re-add the 
+     $("iframe#cnm-iframe").attr('src', src);
+
+   });
